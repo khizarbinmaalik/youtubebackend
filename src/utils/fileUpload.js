92eeac370b4f76af46fbaab0365d1efd4665e7f8
khizarbinmaalik <khizar326199@gmail.com>
2025-09-05 23:42:32 +0500

    setup the cloudinary configuration and Multer

---
+++ src/utils/fileUpload.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+    cloud_name: 'dwqhwnap8',
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+export async function uploadfile(path) {
+    try {
+        // Upload file on cloudinary
+        const uploadResult = await cloudinary.uploader.upload(path, {
+            resource_type: "auto",
+        });
+        // Remove file from local storage
+        fs.unlinkSync(path);
+        return uploadResult;
+    } catch (error) {
+        console.error("Error uploading file:", error);
+        fs.unlinkSync(path);
+        return null;
+    }
+} 
